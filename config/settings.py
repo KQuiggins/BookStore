58279cfbafa0d40ad9942f82f74dadcc33db9eab
kenneth quiggins <kashfamilyvalues@gmail.com>
2022-07-12 19:29:20 -0400

    added registration and test

--- config/settings.py
+++ config/settings.py
@@ -112,4 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODEL = 'accounts.CustomUser' 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
